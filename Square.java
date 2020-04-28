e425e8309b13740452ff7097ebc76e6857333a16
Jemma Siegel <siegelj@gfacademy.org>
2020-04-28 10:31:29 -0400

    Initial sharing of project

---
+++ Square.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Square here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Square
+{
+    private int dimension;
+    private int[][] datarray;
+    public Square(int order)
+    {
+        
+        
+    }
+
+}
